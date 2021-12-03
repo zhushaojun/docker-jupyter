47ed4cd0d5ceb1641b6421ccab62b9d42e44755b
Shaojun Zhu <shj.zhu@gmail.com>
2021-12-03 19:34:02 +0800

    x

--- build_images.sh
+++ build_images.sh
@@ -14,10 +14,11 @@ date=$(date '+%Y%m%d')
 private_registry=172.20.137.125:5000/
 
 tag_and_push_image() {
-	docker push $1
+	docker tag $1 zhushaojun/$1
+	docker push zhushaojun/$1
 
-	docker tag $1 $1-$date
-	docker push $1-$date
+	docker tag $1 zhushaojun/$1-$date
+	docker push zhushaojun/$1-$date
 
 	docker tag $1 $private_registry$1
 	docker push $private_registry$1
@@ -28,13 +29,13 @@ tag_and_push_image() {
 for py in 3.7 3.8 3.9 3.10
 do
 	base_image_name=scipy-cpu
-	base_image_tag=zhushaojun/$base_image_name:py$py
+	base_image_tag=$base_image_name:py$py
 	docker build --build-arg PYTHON_VERSION=$py --tag $base_image_tag ./$base_image_name
 	tag_and_push_image $base_image_tag
 
 	for image_name in auto-sklearn pycaret
 	do
-		tagname=zhushaojun/$image_name:py$py
+		tagname=$image_name:py$py
 		docker build --build-arg BASE_CONTAINER=$base_image_tag --tag $tagname ./$image_name
 		tag_and_push_image $tagname
 	done
@@ -45,13 +46,13 @@ done
 for py in 3.7 3.8 3.9 3.10
 do
 	base_image_name=scipy-gpu
-	base_image_tag=zhushaojun/$base_image_name:py$py
+	base_image_tag=$base_image_name:py$py
 	docker build --build-arg PYTHON_VERSION=$py --tag $base_image_tag ./$base_image_name
 	tag_and_push_image $base_image_tag
 
 	for image_name in mxnet pytorch tensorflow
 	do
-		tagname=zhushaojun/$image_name:py$py
+		tagname=$image_name:py$py
 		docker build --build-arg BASE_CONTAINER=$base_image_tag --tag $tagname ./$image_name
 		tag_and_push_image $tagname
 	done
