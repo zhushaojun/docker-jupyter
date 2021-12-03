60c3c07f8b086a544fa96efc74f2bfd95fa3b525
Shaojun Zhu <shj.zhu@gmail.com>
2021-12-03 11:22:40 +0800

    q

--- build_images.sh
+++ build_images.sh
@@ -12,24 +12,45 @@ docker system prune -y
 #date=$(date '+%Y%m%d-%H%M%S')
 date=$(date '+%Y%m%d')
 # gpu notebook
-for py in 3.7 3.8 3.9
+for py in 3.7 3.8
 do
-	base_image_tag=zhushaojun/cuda-scipy:$py-$date
-	docker build --build-arg PYTHON_VERSION=$py --tag $base_image_tag ./scipy
+	base_image_name=scipy-gpu
+	base_image_tag=zhushaojun/$base_image_name:$py
+	docker build --build-arg PYTHON_VERSION=$py --tag $base_image_tag ./$base_image_name
 	docker push $base_image_tag
+	tag_with_date=$base_image_tag-$date
+	docker tag $base_image_tag $tag_with_date
+	docker push $tag_with_date
 
 	for image_name in mxnet pytorch tensorflow
 	do
-		tagname=zhushaojun/$image_name:$py-$date
+		tagname=zhushaojun/$image_name:$py
 		docker build --build-arg BASE_CONTAINER=$base_image_tag --tag $tagname ./$image_name
 		docker push $tagname
+		tag_with_date=$tagname-$date
+		docker tag $tagname $tag_with_date
+		docker push $tag_with_date
 	done
 done
 
 # cpu notebook
-for image_name in auto-sklearn pycaret
+for py in 3.7 3.8
 do
-	tagname=zhushaojun/$image_name:$date
-	docker build --tag $tagname ./$image_name
-	docker push $tagname
+	base_image_name=scipy-cpu
+	base_image_tag=zhushaojun/$base_image_name:$py
+	docker build --build-arg PYTHON_VERSION=$py --tag $base_image_tag ./$base_image_name
+	docker push $base_image_tag
+	tag_with_date=$base_image_tag-$date
+	docker tag $base_image_tag $tag_with_date
+	docker push $tag_with_date
+
+	for image_name in auto-sklearn pycaret
+	do
+		tagname=zhushaojun/$image_name:$py
+		docker build --build-arg BASE_CONTAINER=$base_image_tag --tag $tagname ./$image_name
+		docker push $tagname
+		tag_with_date=$tagname-$date
+		docker tag $tagname $tag_with_date
+		docker push $tag_with_date
+	done
 done
